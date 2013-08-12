e8676e5e03a7b54f5fb985a6d6f82159314cf6b0
Jamy <jamygolden@gmail.com>
2013-08-12 08:06:59 +0200

    Avoid IE `console` errors
    Avoid `console` errors in browsers that lack a console.

--- src/js/box/debug.js
+++ src/js/box/debug.js
@@ -5,6 +5,27 @@
 	// due to browser security restrictions.
 	if (box.IS_DEBUG)
 	{
+		// Avoid `console` errors in browsers that lack a console.
+		var method;
+		var noop = function () {};
+		var methods = [
+			'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+			'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+			'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+			'timeStamp', 'trace', 'warn'
+		];
+		var length = methods.length;
+		var console = (window.console = window.console || {});
+
+		while (length--) {
+			method = methods[length];
+
+			// Only stub undefined methods.
+			if (!console[method]) {
+				console[method] = noop;
+			}
+		}
+		
 		box.debug = console;
 	}
 	else //nix console logs entirely
